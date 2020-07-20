457ccf52efe57102f05def8f9bd5b6a11ccb8be8
pcs3rd <rhean1620@gmail.com>
2020-07-20 13:15:34 -0400

    Added some stuff, qbcom.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 import qbcom as pyqb
 import xmltodict
+import json
+import socket
 #######################
 #Start some app stuff #
 #######################
@@ -9,14 +11,19 @@ api = FastAPI()
 qb = pyqb.open()
 tk = pyqb.begin(qb)
 #web server
-@api.get("/itemquery/{itemid}")
-async def read_item(itemid: str, q: Optional[str] = None):
-    if q:
-        returndata = xmltodict.parse(pyqb.itemquery(qb, tk, itemid))
-        return returndata
-    return {"Error: no itemid gaven. Gotta give to get."}
+@api.get("/items/{item_id}")
+async def read_item(item_id):
+    returndata = json.dumps(xmltodict.parse(pyqb.itemquery(qb, tk, 43313), dict_constructor=dict, encoding='utf-8'))
+    return returndata
 
 @api.get("/inventorydump")
 async def inventorydump():
-    returndata = pyqb.inventory(qb, tk)
+    returndata = json.dumps(xmltodict.parse(pyqb.inventory(qb, tk), dict_constructor=dict, encoding='utf-8'))
+    return returndata
+
+@api.get("/netinfo")
+async def netinfo():
+    host_name = socket.gethostname()
+    host_ip = socket.gethostbyname(host_name)
+    returndata = host_ip, host_name
     return returndata
